55827a68823d611f66de0c067aee449f7fc46680
root <root@localhost.localdomain>
2026-05-29 13:38:47 +0000

    fix: EpisodeItem missing .clickable

--- app/src/main/java/com/anix/app/ui/components/EpisodeItem.kt
+++ app/src/main/java/com/anix/app/ui/components/EpisodeItem.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.border
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -41,7 +42,8 @@ fun EpisodeItem(
             .fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .border(BorderStroke(2.dp, BorderBlack), RoundedCornerShape(8.dp))
-            .padding(8.dp),
+            .padding(8.dp)
+            .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
