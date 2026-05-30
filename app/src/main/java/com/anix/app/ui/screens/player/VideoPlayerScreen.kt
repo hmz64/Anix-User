626fdaa7d51b1521873dabed07b819b5035a5d68
hmz64 <hmz64@users.noreply.github.com>
2026-05-30 06:43:01 +0000

    Add missing wrapContentHeight import

--- app/src/main/java/com/anix/app/ui/screens/player/VideoPlayerScreen.kt
+++ app/src/main/java/com/anix/app/ui/screens/player/VideoPlayerScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
